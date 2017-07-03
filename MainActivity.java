2d889952eaa2beb0e1cd796456985f6e0f3978a9
jofo777 <jonathan.foureur@gmail.com>
2017-07-03 09:16:35 +0200

    Premiers fichiers

---
+++ MainActivity.java
@@ -0,0 +1,160 @@
+package com.example.jonathan.applicationtest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.widget.Button;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.EditText;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        final Button button = (Button) findViewById(R.id.btn_launch);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onClickBtnLaunch();
+                // Code here executes on main thread after user presses button
+            }
+        });
+
+
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void onClickBtnLaunch(){
+
+        EditText et = (EditText) findViewById(R.id.EditText01);
+        final String str = et.getText().toString();
+
+        EditText et2 = (EditText) findViewById(R.id.EditText02);
+        final String str2 = et2.getText().toString();
+
+        EditText et3 = (EditText) findViewById(R.id.EditText03);
+        final Integer str3 = Integer.parseInt(et3.getText().toString());
+
+
+        Toast toast = Toast.makeText(getApplicationContext(), str+"\n"+str2+"\n"+str3, Toast.LENGTH_SHORT);
+        toast.show();
+
+        Thread t = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    Socket s = new Socket(str2, str3);
+                    PrintWriter out = new PrintWriter(new BufferedWriter(
+                            new OutputStreamWriter(s.getOutputStream())),
+                            true);
+                    out.println(str);
+                    s.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        t.start();
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ server.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 30 18:26:12 2017
+
+@author: jonathan
+"""
+from socket import *
+import socket
+
+##########################
+## Test press bar ########
+from pykeyboard import PyKeyboard
+k = PyKeyboard()
+############################
+
+def find_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
+    local_ip_address = s.getsockname()[0]
+    s.close()
+    return local_ip_address
+
+HOST = find_local_ip() #local host
+PORT = 7000 #open port 7000 for connection
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.bind((HOST, PORT)) #how many connections can it receive at one time
+mysock.listen(1)
+print("Socket created on "+HOST+":"+str(PORT))
+while True:
+    conn, addr = mysock.accept() #accept the connection
+    print("Connected by: " + str(addr)) #print the address of the person connected
+    data = conn.recv(1024) #how many bytes of data will the server receive
+    print("Received: "+ str(data)[2:-3])
+    if(str(data)[2:-3]=="s"):
+        k.press_key(' ')
+        k.release_key(' ')
+        conn.close()
+    elif(str(data)[2:-3]=='q'):
+        conn.close()
+        mysock.close()
+        break
+print("Socket closed")
